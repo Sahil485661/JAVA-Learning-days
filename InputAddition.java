cd0e386685ca7669a5043387eafbf0b8eb4014f3
Sahil Vishwakarma <140201593+Sahil485661@users.noreply.github.com>
2025-07-09 17:10:25 +0530

    Update InputAddition.java

--- InputAddition.java
+++ InputAddition.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.*;
 public class InputAddition
 {
@@ -28,34 +27,3 @@ public class InputAddition
 
     }
 }
-=======
-import java.util.*;
-public class InputAddition
-{
-    public static void main (String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the First Number: ");
-        int a = sc.nextInt();
-        System.out.println("Enter the Second Number: ");
-        int b = sc.nextInt();
-        sc.close();
-        System.out.println("Addition " + (a + b));
-        System.out.println("Subtration " + (a-b));
-        System.out.println("Multiplication " + (a * b));
-        if(b!= 0){
-        System.out.println("Division " + (a / b));
-        System.out.println("Modulus " + (a % b));
-        }
-        else {
-            System.out.println("Division by zeo is not possible");
-            System.out.println("Modulus by zero is not possible");
-        }
-        System.out.println("Pre Increement " + (++a));
-        System.out.println("Post Increement " + (a++));
-        System.out.println("Pre Decreement " + (--b));
-        System.out.println("Post Decreement " + (b--));
-
-    }
-}
->>>>>>> 315e09f0bb94e09d5334acbd0c5a4336eda77592
