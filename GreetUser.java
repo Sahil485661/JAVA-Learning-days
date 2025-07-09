50649ad68f02bd27fa14cf49ad11c8d2d0d9aa1a
Sahil Vishwakarma <140201593+Sahil485661@users.noreply.github.com>
2025-07-09 17:08:54 +0530

    Update GreetUser.java

--- GreetUser.java
+++ GreetUser.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.*;
 public class GreetUser
 {
@@ -11,17 +10,4 @@ public class GreetUser
     }
     
 }
-=======
-import java.util.*;
-public class GreetUser
-{
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        sc.close();
-        System.out.println("Hello Welcome " +name + "!");
-    }
-    
-}
->>>>>>> 315e09f0bb94e09d5334acbd0c5a4336eda77592
+
