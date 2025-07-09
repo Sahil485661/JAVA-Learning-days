9adeb24d50663a485e29136a4b924a75ab23ce64
Sahil485661 <vishwakarmasahil762@gmail.comgit config --global user.email>
2025-07-09 16:43:22 +0530

    first

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+    public static void main(String[] args) 
+    {
+        System.out.print("hello world\n");
+        System.out.println("I am Sahil");
+        System.out.print("I am a Software Developer");
+        System.out.print("\nHello World\nI am sahil\nI am a java developer");
+    }
+}
