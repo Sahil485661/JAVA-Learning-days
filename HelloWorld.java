57bff784d9313fa20c15fa726d77b6665a153abf
Sahil Vishwakarma <140201593+Sahil485661@users.noreply.github.com>
2025-07-09 17:09:41 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class HelloWorld{
     public static void main(String[] args) 
     {
@@ -7,14 +6,4 @@ public class HelloWorld{
         System.out.print("I am a Software Developer");
         System.out.print("\nHello World\nI am sahil\nI am a java developer");
     }
-=======
-public class HelloWorld{
-    public static void main(String[] args) 
-    {
-        System.out.print("hello world\n");
-        System.out.println("I am Sahil");
-        System.out.print("I am a Software Developer");
-        System.out.print("\nHello World\nI am sahil\nI am a java developer");
-    }
->>>>>>> 315e09f0bb94e09d5334acbd0c5a4336eda77592
 }
