a1d566ff2cc20602ff44d189c86b763f22437df7
Sahil485661 <vishwakarmasahil762@gmail.comgit config --global user.email>
2025-07-10 17:38:40 +0530

    basic Loop

---
+++ controlStatement/Loop_Statement.java
@@ -0,0 +1,10 @@
+package controlStatement;
+public class Loop_Statement {
+    public static void main(String[] args) 
+    {
+        String Value = "Sahil is a Software developer";
+        for(int i = 0; i<= 10; i++) {
+            System.out.println( Value);
+        }
+    }
+}
