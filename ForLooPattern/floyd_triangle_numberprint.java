c9fa228773e73b36a0b3f5ba8945fb2f08286bc5
Sahil485661 <vishwakarmasahil762@gmail.comgit config --global user.email>
2025-07-12 18:19:09 +0530

    pattern problem

---
+++ ForLooPattern/floyd_triangle_numberprint.java
@@ -0,0 +1,23 @@
+package ForLooPattern;
+
+public class floyd_triangle_numberprint {
+    public static void main(String[] args){
+        int n = 5;
+        int number = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(number +" ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+  Output: 
+        1
+        2 3
+        4 5 6
+        7 8 9 10
+        11 12 13 14 15
+ */
